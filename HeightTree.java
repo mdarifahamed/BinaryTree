2a72ccc919b84a9e42510db63cf77ffcb49905d1
Arif <arifonly4@gmail.com>
2023-01-15 14:04:12 +0530

    BinaryTree

--- HeightTree.java
+++ HeightTree.java
@@ -32,7 +32,8 @@ public class HeightTree {
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
-        root.right.left = new Node(5);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
         root.right.right = new Node(7); 
 
         System.out.println(height(root));
